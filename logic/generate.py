a81aea4b9a0a1efb719a42699ce0ad62a3641fbf
Markus Schmidgall <markus.schmidgall@googlemail.com>
2023-10-02 00:33:06 +0200

    Split generate_fullstack into subfunctions

--- logic/generate.py
+++ logic/generate.py
@@ -5,6 +5,46 @@ import os
 import tkinter as tk
 
 
+def get_selected_technologies(variables, names):
+    return [name for var, name in zip(variables, names) if var.get() == 1]
+
+
+def create_angular_project(directory, root_directory):
+    try:
+        cmd = f"ng new {directory} --routing=true --style=css --skip-install --skip-git"
+        subprocess.check_call(cmd, cwd=root_directory, shell=True)
+        print(f"Successfully created Angular project {directory}")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error creating Angular project: {e}")
+
+
+def setup_directories(root_directory, selected, directories, docker_selected):
+    for name in selected:
+        directory_path = os.path.join(root_directory, directories[name])
+
+        if name == "Angular":
+            create_angular_project(directories[name], root_directory)
+        else:
+            os.makedirs(directory_path, exist_ok=True)
+
+        if docker_selected:
+            with open(os.path.join(directory_path, "Dockerfile"), "w") as file:
+                file.write(f"# Dockerfile for {name}\n")
+                file.write(
+                    f"# You can add the necessary commands to build the Docker image for {name} here\n"
+                )
+
+
+def create_docker_compose(root_directory, selected, directories):
+    with open(os.path.join(root_directory, "docker-compose.yml"), "w") as file:
+        file.write('version: "3"\n')
+        file.write("services:\n")
+        for name in selected:
+            file.write(f"  {directories[name]}:\n")
+            file.write(f"    build: ./{directories[name]}\n")
+
+
 def generate_fullstack(frontend_var, backend_vars, db_var, docker_var):
     directories = {
         "Angular": "frontend_angular",
@@ -21,50 +61,21 @@ def generate_fullstack(frontend_var, backend_vars, db_var, docker_var):
     root_directory = "fullstack_temp"
     os.makedirs(root_directory, exist_ok=True)
 
-    selected = []
-    for var, name in zip(frontend_var, ["Angular", "React"]):
-        if var.get() == 1:
-            selected.append(name)
+    frontend_selected = get_selected_technologies(frontend_var, ["Angular", "React"])
+    backend_selected = get_selected_technologies(
+        backend_vars, ["Flask", "Django", "Spring Boot"]
+    )
+    db_selected = get_selected_technologies(
+        db_var, ["Neo4J", "MongoDB", "PostgreSQL", "MySQL"]
+    )
 
-    for var, name in zip(backend_vars, ["Flask", "Django", "Spring Boot"]):
-        if var.get() == 1:
-            selected.append(name)
-
-    for var, name in zip(db_var, ["Neo4J", "MongoDB", "PostgreSQL", "MySQL"]):
-        if var.get() == 1:
-            selected.append(name)
-
+    selected = frontend_selected + backend_selected + db_selected
     docker_selected = docker_var.get() == 1
 
-    for name in selected:
-        directory_path = os.path.join(root_directory, directories[name])
-
-        if name == "Angular":
-            # Create Angular project using subprocess
-            try:
-                cmd = f"ng new {directories[name]} --routing=true --style=css --skip-install --skip-git"
-                subprocess.check_call(cmd, cwd=root_directory, shell=True)
-                print(f"Successfully created Angular project {directories[name]}")
-
-            except subprocess.CalledProcessError as e:
-                print(f"Error creating Angular project: {e}")
-        else:
-            os.makedirs(directory_path, exist_ok=True)
-
-        if docker_selected:
-            with open(os.path.join(directory_path, "Dockerfile"), "w") as file:
-                file.write(f"# Dockerfile for {name}\n")
-                file.write(
-                    f"# You can add the necessary commands to build the Docker image for {name} here\n"
-                )
+    setup_directories(root_directory, selected, directories, docker_selected)
 
     if docker_selected:
-        with open(os.path.join(root_directory, "docker-compose.yml"), "w") as file:
-            file.write('version: "3"\n')
-            file.write("services:\n")
-            for name in selected:
-                file.write(f"  {directories[name]}:\n")
-                file.write(f"    build: ./{directories[name]}\n")
+        create_docker_compose(root_directory, selected, directories)
 
     # Saving the entire structure as a zip
     save_directory = filedialog.asksaveasfilename(
